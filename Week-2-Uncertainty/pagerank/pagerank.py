7d06232119d50c41e32a4c578707a2bab79d59bc
Willard Washington Ford <willardwashingtonford@gmail.com>
2023-01-26 18:11:02 -0800

    Completed Markov Chain Model of pagerank algorithm. Works as long as no duplicate links on same page.

--- Week-2-Uncertainty/pagerank/pagerank.py
+++ Week-2-Uncertainty/pagerank/pagerank.py
@@ -7,17 +7,16 @@ import numpy as np
 DAMPING = 0.85
 SAMPLES = 10000
 
+# Haven't set up a system for dealing with identical links on same page yet.
 
 def main():
     if len(sys.argv) != 2:
         sys.exit("Usage: python pagerank.py corpus")
     corpus = crawl(sys.argv[1])
-    '''
     ranks = sample_pagerank(corpus, DAMPING, SAMPLES)
     print(f"PageRank Results from Sampling (n = {SAMPLES})")
     for page in sorted(ranks):
         print(f"  {page}: {ranks[page]:.4f}")
-    '''
     ranks = iterate_pagerank(corpus, DAMPING)
     print(f"PageRank Results from Iteration")
     for page in sorted(ranks):
@@ -60,8 +59,27 @@ def transition_model(corpus, page, damping_factor):
     linked to by `page`. With probability `1 - damping_factor`, choose
     a link at random chosen from all pages in the corpus.
     """
-    raise NotImplementedError
 
+    transition_dist = {}
+
+    minimum_prob = (1-damping_factor)/len(corpus)
+    if len(corpus[page]) != 0:
+        inv_num_links = 1/len(corpus[page])
+        for site in corpus:
+            if site in corpus[page] and site is not page:
+                transition_dist[site] = minimum_prob + damping_factor*inv_num_links
+            else:
+                transition_dist[site] = minimum_prob
+    else:
+        for site in corpus:
+            transition_dist[site] = 1/len(corpus)
+    sum = 0
+    for site in transition_dist.keys():
+        sum += transition_dist[site]
+    if sum < .99:
+        for site in transition_dist.keys():
+            transition_dist[site] = transition_dist[site]/sum
+    return transition_dist
 
 def sample_pagerank(corpus, damping_factor, n):
     """
@@ -72,7 +90,26 @@ def sample_pagerank(corpus, damping_factor, n):
     their estimated PageRank value (a value between 0 and 1). All
     PageRank values should sum to 1.
     """
-    raise NotImplementedError
+
+    visit_counts = {}
+
+    transition_dists = {}
+    for site in corpus:
+        transition_dists[site] = transition_model(corpus, site, damping_factor)
+
+    # Add random for curr_site
+    curr_site = random.choice(list(corpus.keys()))
+    curr_dist = {}
+    for i in range(n):
+        visit_counts[curr_site] = visit_counts.setdefault(curr_site, 0) +1
+        curr_dist = transition_dists[curr_site]
+        curr_site = random.choices(list(curr_dist.keys()), list(curr_dist.values()))[-1]
+        
+
+    probability_dist = {}
+    for site in corpus:
+        probability_dist[site] = visit_counts.setdefault(site, 0)/n
+    return probability_dist
 
 def iterate_pagerank(corpus, damping_factor):
     """
@@ -118,7 +155,10 @@ def find_inputs(corpus): # {a:(c, b), b:(a), c(b)} -> {a:(b), b:(c,a), c:(a)}
 def calculate_weighted(linking_pages, PageRanks, corpus):
     weighted_value = 0
     for page in linking_pages:
-        weighted_value += PageRanks[page]/len(corpus[page])
+        if len(corpus[page]) == 0:
+            weighted_value += PageRanks[page]/len(corpus)
+        else:
+            weighted_value += PageRanks[page]/len(corpus[page])
     return weighted_value
 
 
